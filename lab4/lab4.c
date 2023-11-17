603d62ddef2608bbf2c5b5004987e8ef19732788
Elizaveta Taran <lizataran103@gmail.com>
2023-11-17 19:46:20 +0200

    lab 4 done

--- lab4/lab4.c
+++ lab4/lab4.c
@@ -4,13 +4,19 @@ int main()
 {
     int m, n, i, j;
     int b = 0, k = 0;
+    do
+    {
+        printf("Input the size of first dimension: ");
+        scanf("%d", &m);
+    } while (m <= 0);
 
-    printf("Input the size of first dimension: ");
-    scanf("%d", &m);
-    printf("Input the size of second dimension: ");
-    scanf("%d", &n);
+    do
+    {
+        printf("Input the size of second dimension: ");
+        scanf("%d", &n);
+    } while (n <= 0);
 
-    int arr1[m][n];
+    int arrA[m][n];
 
     printf("Input %d array elements\n", m * n);
 
@@ -19,34 +25,37 @@ int main()
         printf("Input %d elements for the %d row: ", n, i + 1);
         for (j = 0; j < n; j++)
         {
-            scanf("%d", &arr1[i][j]);
+            scanf("%d", &arrA[i][j]);
 
-            if (arr1[i][j] < 0)
+            if (arrA[i][j] > 0)
             {
                 b += 1;
             }
         }
     }
 
-    int arr2[b];
+    int arrB[b];
 
     for (i = 0; i < m; i++)
     {
         for (j = 0; j < n; j++)
         {
-            if (arr1[i][j] < 0)
+            if (arrA[i][j] > 0 && k < b)
             {
-                arr2[k] = arr1[i][j];
+                arrB[k] = arrA[i][j];
                 k += 1;
             }
         }
     }
 
     printf("Result: ");
-    for (k = 0; k < b; k++)
-    {
-        printf("%d  ", arr2[k]);
-    }
+    if (b == 0)
+        printf("There are no positive numbers in array.");
+    else
+        for (k = 0; k < b; k++)
+        {
+            printf("%d  ", arrB[k]);
+        }
 
     return 0;
 }
