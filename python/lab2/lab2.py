2e72728e638f2554ae36ddbae2c9cb6ba1d536c2
Elizaveta Taran <lizataran103@gmail.com>
2024-05-22 18:22:24 +0300

    added lab2

--- python/lab2/lab2.py
+++ python/lab2/lab2.py
@@ -1,12 +1,20 @@
 while True: 
-    operation_type = input("Enter one of the following types of operations: ** or 1:7\nTo end the program enter 'e' character\n")
-    if operation_type == "e":
+    operation_type = input("Enter one of the following types of operations: ** or 1:7\nTo end the program enter 'q' character\n")
+
+    if operation_type == "q":
+        print("See you later!\n")
         break
-    number = int(input("Enter a number to operate with: "))
+
+    if operation_type != "**" and operation_type != "1:7":
+        print("Error! Incorrect type of operation\n")
+        continue
+
+    number = int(input("Enter an integer number to operate with: "))
+
     if operation_type == "**":
-        exponent = int(input("Enter a number of exponent: "))
-        print(pow(number, exponent))
-    elif operation_type == "1:7":
+        exponent = int(input("Enter an integer number of exponent: "))
+        print(number ** exponent)
+    else:
         result = ""
         for i in str(number):
             if i == "1":
@@ -14,5 +22,5 @@ while True:
             else:
                 result += i
         print(result)
-    else:
-        print("Incorrect type of operation")
+
+    print("Success!\n")
