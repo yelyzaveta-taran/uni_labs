e1676d252c42e3a2dc32047c80783346890ea6ed
Elizaveta Taran <lizataran103@gmail.com>
2024-05-22 14:25:10 +0300

    added python lab1

---
+++ python/lab1/main.py
@@ -0,0 +1,5 @@
+car_make = input("What's the make of the car?\n")
+engine_type = input("What type of engine?\n")
+car_price = input("How much does the car cost?\n")
+
+print(f"{car_make} with a {engine_type} engine costs {car_price}$.")
