2fbcb4cbf80a78c30c2b18e0b77140561adeda8e
Elizaveta Taran <lizataran103@gmail.com>
2024-05-29 16:47:29 +0300

    finished GUI

--- python/lab3/gui.py
+++ python/lab3/gui.py
@@ -1,29 +1,11 @@
 from tkinter import *
-from tkinter import messagebox
+from tkinter import ttk
 from main import Circle, Rectangle, Square
-
-root = Tk()
-root.title("Graphic program in Python")
-
-w = 500
-h = 600
-
-ws = root.winfo_screenwidth() 
-hs = root.winfo_screenheight() 
-
-x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
-
-root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-for i in range(5):
-    root.grid_columnconfigure(i, weight=1)
-
+from helpers import *
 
 counter = 0
 colors = ["yellow", "lawn green", "medium orchid"]
 shapes = [("Circle", 1), ("Rectangle", 2), ("Square", 3)]
-
 
 def display_elements():
     shape_label.grid_forget()
@@ -51,40 +33,6 @@ def display_elements():
 
     submit_button.grid(row=3, column=3, padx=(5, 15), pady=10, sticky="we")
 
-def format_dimensions(dimensions):
-    formatted_dimensions = []
-    for num in dimensions:
-        num = num.replace(",", ".")
-        try:
-            formatted_dimensions.append(float(num))
-        except ValueError:
-            return None
-    return formatted_dimensions
-
-def input_validation(shape, dimensions):
-    if dimensions is None:
-        messagebox.showerror(title="Validation Error", message="Value must be a number")
-        return False
-    
-    if not dimensions: 
-        messagebox.showerror(title="Validation Error", message="Value cannot be an empty string")
-        return False
-
-    if shape == 2 and len(dimensions) != 2:
-        messagebox.showerror(title="Validation Error", message="For a rectangle, enter two numbers separated by a space")
-        return False
-   
-    if shape != 2 and len(dimensions) > 1:
-        messagebox.showerror(title="Validation Error", message="Enter just one number to calculate the area and perimeter of a square or circle.")
-        return False
-
-    for num in dimensions:
-        if num <= 0:
-            messagebox.showerror(title="Validation Error", message="Value must be a positive number")
-            return False
-    
-    return True
-
 def on_btn_submit():
     global counter 
     counter = 0
@@ -108,21 +56,22 @@ def on_btn_submit():
         match selected_shape:
             case 1:
                 shape = Circle(dimensions[0])
-                canvas.create_oval(3, 3, 200, 200, fill=colors[0],  outline='black', width=3, activedash=(5, 4))
             case 2: 
                 shape = Rectangle(dimensions[0], dimensions[1])
-                canvas.create_rectangle(3, 50, 200, 150, fill=colors[0],  outline='black', width=3, activedash=(5, 4))
             case 3:
                 shape = Square(dimensions[0])
-                canvas.create_rectangle(3, 3, 200, 200, fill=colors[0],  outline='black', width=3, activedash=(5, 4))
             case _:
                 raise Exception("Invalid type of shape")    
 
     if shape:
         shape_label.grid(row=4, column=0, columnspan=5, sticky="s")
         shape_label.config(text=str(shape))
+
+        draw_image(canvas, colors, selected_shape)
+
         prev_button.grid(row=0, column=1, padx=(0, 10), sticky="we")
         prev_button.config(state="disabled")
+        
         next_button.grid(row=0, column=2, padx=(10, 0), sticky="we")
         next_button.config(state="normal")
 
@@ -144,30 +93,45 @@ def on_btn_click(btn_text):
     elif counter == 2:
         next_button.config(state="disabled")
 
-    match selected_shape:
-        case 1: 
-            canvas.create_oval(3, 3, 200, 200, fill=colors[counter],  outline='black', width=3, activedash=(5, 4))
-        case 2:
-            canvas.create_rectangle(3, 50, 200, 150, fill=colors[counter],  outline='black', width=3, activedash=(5, 4))
-        case 3: 
-            canvas.create_rectangle(3, 3, 200, 200, fill=colors[counter],  outline='black', width=3, activedash=(5, 4))
+    draw_image(canvas, colors, selected_shape, counter)
+
+root = Tk()
+root.title("GUI in Python")
+
+w = 700
+h = 550
+
+ws = root.winfo_screenwidth() 
+hs = root.winfo_screenheight() 
+
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+for i in range(5):
+    root.grid_columnconfigure(i, weight=1)
 
 header = Label(text="Choose a shape:", font=14)
 header.grid(row=0, column=0, padx=15, pady=15, columnspan=5, sticky=W)
 
+radio_frame = Frame(root)
+radio_frame.grid(row=1, column=0, columnspan=5, sticky="we")
+for i in range(4):
+    radio_frame.grid_columnconfigure(i, weight=1)
+
 shape_int = IntVar()
 column = 1
-
 for txt, val in shapes:
-    Radiobutton(text=txt, value=val,  variable=shape_int, command=display_elements).grid(row=1, padx=15, pady=10, column=column, sticky="w")
+    ttk.Radiobutton(radio_frame, text=txt, value=val,  variable=shape_int, command=display_elements).grid(row=1, padx=15, pady=10, column=column, sticky="we")
     column += 1
 
 dimensions_label = Label(font=14)
 
 dimensions_value = StringVar()
-dimensions_input = Entry(textvariable=dimensions_value, width=28, font=12)
+dimensions_input = ttk.Entry(textvariable=dimensions_value, width=28, font=12)
 
-submit_button = Button(text="Submit", command=on_btn_submit)
+submit_button = ttk.Button(text="Submit", command=on_btn_submit)
 
 shape_label = Label(padx=5, pady=5, font=("Calibri", 14, "bold"))
 
@@ -175,14 +139,11 @@ canvas = Canvas(root, width=200, height=200)
 
 button_frame = Frame(root)
 button_frame.grid(row=7, column=0, columnspan=5, sticky="we")
-button_frame.grid_columnconfigure(0, weight=1)
-button_frame.grid_columnconfigure(1, weight=1)
-button_frame.grid_columnconfigure(2, weight=1)
-button_frame.grid_columnconfigure(3, weight=1)
+for i in range(4):
+    button_frame.grid_columnconfigure(i, weight=1)
 
-
-prev_button = Button(button_frame, text="Previous", command=lambda: on_btn_click(prev_button.cget("text")), state="disabled")
-next_button = Button(button_frame, text="Next", command=lambda: on_btn_click(next_button.cget("text")))
+prev_button = ttk.Button(button_frame, text="Previous", command=lambda: on_btn_click(prev_button.cget("text")), state="disabled")
+next_button = ttk.Button(button_frame, text="Next", command=lambda: on_btn_click(next_button.cget("text")))
 
 
 root.mainloop()
