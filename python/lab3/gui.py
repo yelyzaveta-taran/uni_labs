5b4fad1632655b56e3bf5527e3d5a83318ec71e1
Elizaveta Taran <lizataran103@gmail.com>
2024-05-29 05:05:39 +0300

    finished the GUI

--- python/lab3/gui.py
+++ python/lab3/gui.py
@@ -1,9 +1,37 @@
 from tkinter import *
+from tkinter import messagebox
 from main import Circle, Rectangle, Square
 
+root = Tk()
+root.title("Graphic program in Python")
+
+w = 500
+h = 600
+
+ws = root.winfo_screenwidth() 
+hs = root.winfo_screenheight() 
+
+x = (ws/2) - (w/2)
+y = (hs/2) - (h/2)
+
+root.geometry('%dx%d+%d+%d' % (w, h, x, y))
+
+for i in range(5):
+    root.grid_columnconfigure(i, weight=1)
+
+
+counter = 0
+colors = ["yellow", "lawn green", "medium orchid"]
+shapes = [("Circle", 1), ("Rectangle", 2), ("Square", 3)]
+
+
 def display_elements():
-    validation_label.config(text="")
-    shape_label.config(text="")
+    shape_label.grid_forget()
+    canvas.grid_forget()
+    prev_button.grid_forget()
+    next_button.grid_forget()
+
+    dimensions_label.grid(row=2, column=0, padx=15, pady=15, columnspan=5, sticky=W)
 
     selected_shape = shape_int.get()
 
@@ -18,10 +46,10 @@ def display_elements():
         dimensions_label.config(text="Enter the side length:")
     
     dimensions_input.delete(0, END)
-    dimensions_input.grid(row=3, column=0, columnspan=4, padx=(15, 5), pady=10, sticky=W)
+    dimensions_input.grid(row=3, column=0, columnspan=3, padx=(15, 5), pady=10, sticky="we")
     dimensions_input.insert(0, placeholder)
 
-    submit_button.grid(row=3, column=2, padx=(5, 15), pady=10, sticky=W)
+    submit_button.grid(row=3, column=3, padx=(5, 15), pady=10, sticky="we")
 
 def format_dimensions(dimensions):
     formatted_dimensions = []
@@ -34,29 +62,40 @@ def format_dimensions(dimensions):
     return formatted_dimensions
 
 def input_validation(shape, dimensions):
-    shape_label.config(text="")
     if dimensions is None:
-        validation_label.config(text="Value must be a number", foreground="red")
+        messagebox.showerror(title="Validation Error", message="Value must be a number")
         return False
     
     if not dimensions: 
-        validation_label.config(text="Value cannot be an empty string", foreground="red")
+        messagebox.showerror(title="Validation Error", message="Value cannot be an empty string")
         return False
 
     if shape == 2 and len(dimensions) != 2:
-        validation_label.config(text="For a rectangle, enter two numbers separated by a space", foreground="red")
+        messagebox.showerror(title="Validation Error", message="For a rectangle, enter two numbers separated by a space")
+        return False
+   
+    if shape != 2 and len(dimensions) > 1:
+        messagebox.showerror(title="Validation Error", message="Enter just one number to calculate the area and perimeter of a square or circle.")
         return False
 
     for num in dimensions:
         if num <= 0:
-            validation_label.config(text="Value must be a positive number", foreground="red")
+            messagebox.showerror(title="Validation Error", message="Value must be a positive number")
             return False
     
-
-    validation_label.config(text="")
     return True
 
 def on_btn_submit():
+    global counter 
+    counter = 0
+
+    shape_label.grid_forget()
+    canvas.grid_forget()
+    prev_button.grid_forget()
+    next_button.grid_forget()
+
+    canvas.delete("all")
+
     selected_shape = shape_int.get()
     str_dimensions = dimensions_input.get().split()
 
@@ -65,85 +104,86 @@ def on_btn_submit():
     shape = {}
 
     if input_validation(selected_shape, dimensions):
+        canvas.grid(row=6, column=0, columnspan=5, pady=20, sticky="s")
         match selected_shape:
             case 1:
                 shape = Circle(dimensions[0])
+                canvas.create_oval(3, 3, 200, 200, fill=colors[0],  outline='black', width=3, activedash=(5, 4))
             case 2: 
                 shape = Rectangle(dimensions[0], dimensions[1])
+                canvas.create_rectangle(3, 50, 200, 150, fill=colors[0],  outline='black', width=3, activedash=(5, 4))
             case 3:
                 shape = Square(dimensions[0])
+                canvas.create_rectangle(3, 3, 200, 200, fill=colors[0],  outline='black', width=3, activedash=(5, 4))
             case _:
-                raise Exception("Invalid type of shape")
-    
+                raise Exception("Invalid type of shape")    
 
-    if shape: shape_label.config(text=str(shape))
+    if shape:
+        shape_label.grid(row=4, column=0, columnspan=5, sticky="s")
+        shape_label.config(text=str(shape))
+        prev_button.grid(row=0, column=1, padx=(0, 10), sticky="we")
+        prev_button.config(state="disabled")
+        next_button.grid(row=0, column=2, padx=(10, 0), sticky="we")
+        next_button.config(state="normal")
 
-def on_btn_click():
+def on_btn_click(btn_text):
+    global counter, colors
+
     selected_shape = shape_int.get()
-    colors = ["d6ff79", "b0ff92", "5f00ba"]
-    btn_text = previous_button.cget('text')
 
+    if btn_text == "Next":
+        counter += 1
+    else:
+        counter -= 1
 
+    if counter == 0:
+        prev_button.config(state="disabled")
+    elif counter > 0 and counter != 2: 
+        prev_button.config(state="normal")
+        next_button.config(state="normal")
+    elif counter == 2:
+        next_button.config(state="disabled")
 
-root = Tk()
-root.title("Graphic program in Python")
+    match selected_shape:
+        case 1: 
+            canvas.create_oval(3, 3, 200, 200, fill=colors[counter],  outline='black', width=3, activedash=(5, 4))
+        case 2:
+            canvas.create_rectangle(3, 50, 200, 150, fill=colors[counter],  outline='black', width=3, activedash=(5, 4))
+        case 3: 
+            canvas.create_rectangle(3, 3, 200, 200, fill=colors[counter],  outline='black', width=3, activedash=(5, 4))
 
-w = 450
-h = 500
-
-ws = root.winfo_screenwidth() 
-hs = root.winfo_screenheight() 
-
-x = (ws/2) - (w/2)
-y = (hs/2) - (h/2)
-
-root.geometry('%dx%d+%d+%d' % (w, h, x, y))
-
-for i in range(4):
-    root.grid_columnconfigure(i, weight=1)
-
-shapes = [("Circle", 1), ("Rectangle", 2), ("Square", 3)]
-
-header = Label(text="Choose a shape:", padx=15, pady=10, font=14)
-header.grid(row=0, column=0, columnspan=4, sticky=W)
+header = Label(text="Choose a shape:", font=14)
+header.grid(row=0, column=0, padx=15, pady=15, columnspan=5, sticky=W)
 
 shape_int = IntVar()
-column = 0
+column = 1
 
 for txt, val in shapes:
-    Radiobutton(text=txt, value=val, borderwidth=4, variable=shape_int, padx=15, pady=10, command=display_elements).grid(row=1, column=column, sticky=W)
+    Radiobutton(text=txt, value=val,  variable=shape_int, command=display_elements).grid(row=1, padx=15, pady=10, column=column, sticky="w")
     column += 1
 
-dimensions_label = Label(padx=15, pady=10, font=14)
-dimensions_label.grid(row=2, column=0, columnspan=5, sticky=W)
+dimensions_label = Label(font=14)
 
 dimensions_value = StringVar()
 dimensions_input = Entry(textvariable=dimensions_value, width=28, font=12)
 
 submit_button = Button(text="Submit", command=on_btn_submit)
 
-validation_label = Label(padx=15, font=14)
-validation_label.grid(row=4, column=0, columnspan=5, sticky=W)
-
 shape_label = Label(padx=5, pady=5, font=("Calibri", 14, "bold"))
-shape_label.grid(row=5, column=0, columnspan=5, sticky="s")
 
 canvas = Canvas(root, width=200, height=200)
-canvas.grid(row=6, column=0, columnspan=5, pady=20, sticky="s")
 
-# canvas.create_rectangle(3, 3, 200, 200, fill='lightpink',  outline='black',
-#                    width=3,
-#                    activedash=(5, 4)) 
+button_frame = Frame(root)
+button_frame.grid(row=7, column=0, columnspan=5, sticky="we")
+button_frame.grid_columnconfigure(0, weight=1)
+button_frame.grid_columnconfigure(1, weight=1)
+button_frame.grid_columnconfigure(2, weight=1)
+button_frame.grid_columnconfigure(3, weight=1)
 
-# canvas.create_rectangle(3, 50, 200, 150, fill='lightpink',  outline='black',
-#                    width=3,
-#                    activedash=(5, 4))
 
-# canvas.create_oval(3, 3, 200, 200, fill='lightpink',  outline='black',
-#                    width=3,
-#                    activedash=(5, 4))
+prev_button = Button(button_frame, text="Previous", command=lambda: on_btn_click(prev_button.cget("text")), state="disabled")
+next_button = Button(button_frame, text="Next", command=lambda: on_btn_click(next_button.cget("text")))
 
-previous_button  = Button(text="Previous", command=on_btn_click)
-previous_button.grid(row=7, column=0)
+
 root.mainloop()
 
